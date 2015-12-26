6ec87a9f7e1526eb08bbef9ae9dedb775b4d70ed
carlosfernandez <prof.carlos.tessier@gmail.com>
2015-12-26 18:51:34 +0100

    added parentActivityName to SignUpActivity

--- app/src/main/java/es/carlostessier/yep2016/SignUpActivity.java
+++ app/src/main/java/es/carlostessier/yep2016/SignUpActivity.java
@@ -1,13 +1,79 @@
 package es.carlostessier.yep2016;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    final static String TAG = SignUpActivity.class.getSimpleName();
+
+    private EditText usernameField;
+    private EditText passwordField;
+    private EditText emailAddressField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        initializeViews();
+
+    }
+
+    private void initializeViews() {
+        usernameField = (EditText) findViewById(R.id.usernameField);
+        passwordField = (EditText) findViewById(R.id.passwordField);
+        emailAddressField = (EditText) findViewById(R.id.emailField);
+    }
+
+    public void actionSignupButton(View v){
+
+        String sUsername = usernameField.getText().toString().trim();
+        String sPassword = passwordField.getText().toString().trim();
+        String sEmailAddress = emailAddressField.getText().toString().trim();
+
+
+        String message ;
+
+        if (sUsername.isEmpty())
+        {
+            message = String.format(getString(R.string.empty_field_message),getString(R.string.username_hint));
+            errorFieldDialog(message);
+        }
+        else if (sPassword.isEmpty())
+        {
+            message = String.format(getString(R.string.empty_field_message),getString(R.string.password_hint));
+            errorFieldDialog(message);
+
+        }
+        else if (sEmailAddress.isEmpty())
+        {
+            message = String.format(getString(R.string.empty_field_message),getString(R.string.email_hint));
+            errorFieldDialog(message);
+
+        }
+        else{
+            Log.d(TAG,"OK");
+
+        }
+
+
+
+    }
+
+
+    private void errorFieldDialog(String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+        builder.setMessage(message);
+        builder.setPositiveButton(android.R.string.ok, null);
+        builder.setTitle(R.string.signup_error_title);
+        builder.setIcon(android.R.drawable.ic_dialog_alert);
+        AlertDialog dialog = builder.create();
+
+        dialog.show();
+
     }
 }
