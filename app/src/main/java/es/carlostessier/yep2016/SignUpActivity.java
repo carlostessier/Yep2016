7ea415bb665f23480b51076a0da1cdf3b8599457
carlosfernandez <prof.carlos.tessier@gmail.com>
2015-12-26 19:01:08 +0100

    create user in parse with SignUpActivity

--- app/src/main/java/es/carlostessier/yep2016/SignUpActivity.java
+++ app/src/main/java/es/carlostessier/yep2016/SignUpActivity.java
@@ -1,11 +1,15 @@
 package es.carlostessier.yep2016;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -56,7 +60,7 @@ public class SignUpActivity extends AppCompatActivity {
 
         }
         else{
-            Log.d(TAG,"OK");
+            addUser(sUsername,sPassword,sEmailAddress);
 
         }
 
@@ -75,5 +79,29 @@ public class SignUpActivity extends AppCompatActivity {
 
         dialog.show();
 
+    }
+
+    private void addUser(final String username, String password, String email) {
+
+        ParseUser user = new ParseUser();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setEmail(email);
+
+
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                } else {
+                    String message = String.format(getString(R.string.add_user_error_message), username);
+                    errorFieldDialog(message);
+                }
+
+            }
+        });
     }
 }
