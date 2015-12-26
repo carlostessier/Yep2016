76cd6f170d7bce7b389bf833fea4c032e7d02b8c
carlosfernandez <prof.carlos.tessier@gmail.com>
2015-12-26 20:50:21 +0100

    added parentActivityName to SignUpActivity

---
+++ app/src/main/java/es/carlostessier/yep2016/InboxFragment.java
@@ -0,0 +1,31 @@
+package es.carlostessier.yep2016;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+/**
+ * Created by carlosfernandez on 26/12/15.
+ * List of messages
+ */
+public class InboxFragment extends android.support.v4.app.ListFragment{
+    ProgressBar spinner;
+
+
+    final static String TAG = InboxFragment.class.getName();
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_inbox, container, false);
+
+        spinner = (ProgressBar)
+                rootView.findViewById(R.id.progressBar);
+        spinner.setVisibility(View.GONE);
+
+        return rootView;
+    }
+}
+
